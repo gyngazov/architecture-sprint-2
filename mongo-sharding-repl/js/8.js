e539293f9f9ca48b46cc75f5bf89322615fdf42d
Leo <l.gyngazov@yandex.ru>
2024-08-23 13:12:11 +0300

    mongo-sharding-repl

---
+++ mongo-sharding-repl/js/8.js
@@ -0,0 +1,10 @@
+rs.initiate(
+    {
+      _id : "shard2",
+      members: [ 
+          { _id : 0, host : "mongodb1-1:27021" },
+          { _id : 1, host : "mongodb1-2:27022" }, 
+          { _id : 2, host : "mongodb1-3:27023" }
+      ]
+    }
+);
